27b9f217793f21a8ce56cdc452806f3cc7b6a5a8
Oleg Snegov <oleg@x2y.io>
2020-02-08 02:11:36 -0500

    X2Y-00: added title binding

--- src/components/common/Menu.vue
+++ src/components/common/Menu.vue
@@ -3,7 +3,7 @@
         <div class="inner">
             <ul class="links">
                 <li @click="close()" v-for="(item, index) in menu" :key="index">
-                    <router-link :to="item.path">{{item.name}}</router-link>
+                    <router-link :to="item.path" :title="item.name">{{item.name}}</router-link>
                 </li>
             </ul>
         </div>
